8120da90916c85a9f1d139b986454846ecec6508
Luis Miguel Ríos Pérez <luismrios1602@gmail.com>
2022-11-07 01:01:52 -0500

    Cambios para la version 2.2
    + Agregar icono de la App.

--- app/src/main/java/com/example/comprameste/MainActivity.java
+++ app/src/main/java/com/example/comprameste/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.comprameste;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
